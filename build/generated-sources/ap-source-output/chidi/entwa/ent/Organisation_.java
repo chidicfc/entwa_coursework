f94054f4ce4dddc7d5093bdd7f952182d8109151
chidi.uba <talk2chidicfc@yahoo.com>
2016-03-03 19:15:46 +0000

    get only active organisations

--- build/generated-sources/ap-source-output/chidi/entwa/ent/Organisation_.java
+++ build/generated-sources/ap-source-output/chidi/entwa/ent/Organisation_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.SetAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-03-03T17:01:19")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-03-03T19:10:44")
 @StaticMetamodel(Organisation.class)
 public class Organisation_ { 
 
