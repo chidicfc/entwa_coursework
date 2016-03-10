c3fa04f7c314cae1a326650c8fcbe76898fd9312
chidi.uba <talk2chidicfc@yahoo.com>
2016-03-10 16:38:56 +0000

    hide actions of organisations I don't own

--- build/generated-sources/ap-source-output/chidi/entwa/ent/UserGroup_.java
+++ build/generated-sources/ap-source-output/chidi/entwa/ent/UserGroup_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-03-10T10:23:20")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-03-10T16:21:05")
 @StaticMetamodel(UserGroup.class)
 public class UserGroup_ { 
 
