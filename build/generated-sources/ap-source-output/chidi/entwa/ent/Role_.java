f465853debabe3152db618685f4f61d1191e81fd
chidi.uba <talk2chidicfc@yahoo.com>
2016-03-13 08:34:21 +0000

    remove omnifaces

--- build/generated-sources/ap-source-output/chidi/entwa/ent/Role_.java
+++ build/generated-sources/ap-source-output/chidi/entwa/ent/Role_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-03-13T04:10:31")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-03-13T08:33:47")
 @StaticMetamodel(Role.class)
 public class Role_ { 
 
